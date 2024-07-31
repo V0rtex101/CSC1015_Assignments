08f48953a14dd3231cce11014cea5518edbeef00
Fulufhelo Mulaudzi <fulumula078@gmail.com>
2024-07-31 12:47:29 +0200

    Removed redudant wildcard function in contacts.py

--- Assignment 11/contacts.py
+++ Assignment 11/contacts.py
@@ -51,56 +51,6 @@ def custom_sort(contacts):
     return contacts
 
 
-def wildcard(pattern, word):
-    """This function takes two arguments: a pattern and a word and then checks
-        whether they are a match or not"""
-
-    # First base case: checks if both pattern and word are empty
-    if not word and not pattern:
-        return True
-
-    # Second base case: checks if one is empty and the other is not
-    if not word or not pattern:
-        return False
-
-    # Third base case: checks if word is empty and pattern is '*'
-    if not word and pattern == '*':
-        return True
-
-    # This section makes use of recursion to remove the first char and move on to the next if conditions are satisfied
-
-    # This if checks if it is '*' and then compares the length of the word and pattern
-    if pattern[0] == '*':
-
-        if len(word) > 1 and len(pattern) > 1:
-            # If second chars of both are equal, remove the first chars
-            if pattern[1] == word[1]:
-                return wildcard(pattern[1:], word[1:])
-            elif pattern[1] == word[0]:
-                return wildcard(pattern[1:], word)
-
-        # If the lengths are equal then the first chars are removed and program continues
-        if len(word) == len(pattern):
-            return wildcard(pattern[1:], word[1:])
-
-        # If word is less than pattern then the first char of pattern is removed
-        elif len(word) < len(pattern):
-            return wildcard(pattern[1:], word)
-
-        # If the previous conditions are false, the first char of the word is removed until one condition is true
-        else:
-            return wildcard(pattern, word[1:])
-
-    # This if checks if it is '?' and since it can be any char, it moves on to the next char
-    if pattern[0] == '?':
-        return wildcard(pattern[1:], word[1:])
-
-    # This if checks if the chars are the same and then moves on to the next char
-    elif word[0] == pattern[0]:
-        return wildcard(pattern[1:], word[1:])
-    return False
-
-
 def ternary_search(data, item, left, right):
     """This function uses the ternary search algorithm in a recursive form to search
      for an item and returns its index if found"""
